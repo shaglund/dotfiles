520f153f8bdc64444ab46e26d708b332855c6f83
Sebastian Haglund <sebastian.haglund@broadridge.com>
2025-09-29 14:58:18 +0200

    Remove bufferline workaround in nvim config

--- nvim/lua/plugins/lazyvim.lua
+++ nvim/lua/plugins/lazyvim.lua
@@ -3,15 +3,6 @@ return {
         "LazyVim/LazyVim",
         opts = {
             colorscheme = "catppuccin",
-        }
     },
-    {
-        "akinsho/bufferline.nvim",
-        init = function()
-            local bufline = require("catppuccin.groups.integrations.bufferline")
-            function bufline.get()
-                return bufline.get_theme()
-            end
-        end,
-    }
+    },
 }
